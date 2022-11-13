5180c920400348e2c879b9977ae8be196992cd11
ferencprem2 <fercsusz0508@gmail.com>
2022-11-13 21:41:37 +0100

    js is done

--- order.js
+++ order.js
@@ -1,46 +1,71 @@
 const foodDivs = document.querySelectorAll(".etelek");
 var cartVisibility = document.getElementById("rendeles");
 cartVisibility.style.visibility = "hidden";
-var cart = [];
-var addedIds = [];
 
+let leves = null
+let levesId =  null
+
+let foetel = null
+let foetelId = null
+
+let desszert = null
+let desszertId =  null
+
+function SetDefaultBackground(id) {
+  if (id != null) {
+    document.getElementById(id).style.backgroundColor = "#A59B91";
+  }
+}
 
 foodDivs.forEach(foodDiv => {
   foodDiv.addEventListener("click", (e) => {
-    cartVisibility.style.visibility = "visible";
-      if (addedIds.includes(e.target.id) == true){
-        alert("A termék már hozzá van adva")
-      } else{
-        cart.push(foodDiv.querySelectorAll("p"));
-        renderCart();
-        addedIds.pop();
-        addedIds.push(e.target.id)
-        alert("id")
-      }
-      
-
-      // cart.push(foodDiv.querySelectorAll("p"));
-      // renderCart();
-      // alert(e.target.id)
-      // alert("a termék már hozzá lett adva");
-      
-
-
+    foodDiv.style.backgroundColor = "lightblue";
+    switch (foodDiv.title) {
+      case "leves":
+        if (foodDiv.id == levesId) {
+          alert("A termék már hozzá van adva")
+        } else {
+          SetDefaultBackground(levesId);
+          leves = foodDiv;
+          levesId = foodDiv.id;
+        }
+        break;
+      case "foetel":
+        if (foodDiv.id == foetelId) {
+          alert("A termék már hozzá van adva")
+        } else {
+          SetDefaultBackground(foetelId);
+          foetel = foodDiv;
+          foetelId = foodDiv.id;
+        }
+        break;
+      case "desszert":
+        if (foodDiv.id == desszertId) {
+          alert("A termék már hozzá van adva")
+        }else {
+          SetDefaultBackground(desszertId);
+          desszert = foodDiv;
+          desszertId = foodDiv.id;
+        }
+        break;
+    }
+    renderCart();
   })
 })
 
-
-
-
-
-// reload cart's content in kosar div
 function renderCart() {
-  let kosar = document.getElementById("kosar")
-  kosar.innerHTML = ""
-  cart.forEach(item => {
-    console.log(item);
-    kosar.innerHTML += `<div>Étel neve: ${item[0].innerHTML}, ár: ${item[1].innerHTML}</div>`
-  })
+  const kosar = document.getElementById("kosar");
+  kosar.innerHTML = "";
+  cartVisibility.style.visibility = "visible";
+  if (levesId != null) {
+    kosar.innerHTML += `<div>Leves: ${leves.querySelectorAll('p')[0].innerHTML}, ár: ${leves.querySelectorAll('p')[1].innerHTML}</div>`
+  }
+  if (foetelId != null) {
+    kosar.innerHTML += `<div>Főétel: ${foetel.querySelectorAll('p')[0].innerHTML}, ár: ${foetel.querySelectorAll('p')[1].innerHTML}</div>`
+  }
+  if (desszertId != null) {
+    kosar.innerHTML += `<div>Desszert: ${desszert.querySelectorAll('p')[0].innerHTML}, ár: ${desszert.querySelectorAll('p')[1].innerHTML}</div>`
+  }
 }
 
 
@@ -50,7 +75,6 @@ document.getElementById("clearCart").addEventListener("click", () => {
     alert("A kosár tartalma eltávolítva")
     cart.splice(0, cart.length);
     renderCart();
-    cartVisibility.style.visibility = "hidden";
   } else {
     alert("A kosár tartalma nem lett eltávolítva")
   }
@@ -63,5 +87,5 @@ document.getElementById("submitCartItems").addEventListener("click", () => {
   cart.splice(0, cart.length);
   renderCart();
   cartVisibility.style.visibility = "hidden";
-  
+
 })
